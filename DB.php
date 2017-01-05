11690fb66c92bdff427524026e09985e3670e28d
jim <liu1084@163.com>
2017-01-05 23:44:30 +0800

    update

---
+++ DB.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jim
+ * Date: 2016/11/22
+ * Time: 16:49
+ */
+class DB
+{
+    public function insert($tableName, $object){
+        //$sql = 'INSERT INTO ' . $;
+        foreach($object as $key => $value){
+
+        }
+    }
+}
